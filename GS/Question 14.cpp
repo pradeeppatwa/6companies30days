d9cfc4f53a90b778586411967cd8783998ef6054
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-07 17:11:03 +0530

    Update Question 14.cpp

--- GS/Question 14.cpp
+++ GS/Question 14.cpp
@@ -1,1 +1,24 @@
+//209. Minimum Size Subarray Sum
+//https://leetcode.com/problems/minimum-size-subarray-sum/submissions/
 
+
+// Solution :
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MAX;  // INT_MAX is a macro which represents the maximum integer value. 
+        int left = 0;
+        int sum = 0;
+        for ( int i = 0 ; i <n ; i++){
+            sum +=nums[i];
+            while( sum >= target){      
+                ans = min (ans, i-left+1);    //sliding window i is end and left is start pointer
+                sum -= nums[left++];
+            }
+        }
+        
+        return( ans != INT_MAX) ? ans : 0;
+    }
+};
