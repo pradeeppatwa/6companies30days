f3d2c6170ad4ca1c0696666219e28c9b793034d8
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-10 00:27:32 +0530

    Create Question 7.cpp

--- Amazon/Question 7.cpp
+++ Amazon/Question 7.cpp
@@ -1,2 +1,31 @@
 //First non-repeating character in a stream
 //https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+
+//solution :
+
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    queue<int> q;
+		    string temp="";
+		    int arr[26]={0};
+		    for(int i=0; i<A.size() ; i++){
+		        arr[A[i]-'a']++;
+		        if(arr[A[i]-'a']==1){        //aa-a != 1
+		            q.push(A[i]);
+		        }
+		        while(!q.empty() && arr[q.front()-'a']!=1){
+		            q.pop();
+		        }
+		        if(q.empty()){
+		            A[i]='#';
+		        }else{
+		            A[i]=q.front();
+		        }
+		    }
+		    return A;
+		    
+		}
+
+};
