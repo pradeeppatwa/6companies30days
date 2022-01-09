bbdaf5331fc478e0c79c49b991a61048adce7bc9
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-09 15:52:56 +0530

    added solution

--- Amazon/Question 3.cpp
+++ Amazon/Question 3.cpp
@@ -1,2 +1,26 @@
 //IPL 2021 - Match Day 2
 //https://practice.geeksforgeeks.org/problems/deee0e8cf9910e7219f663c18d6d640ea0b87f87/1/
+
+//Solution :
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        multiset<int> st;
+        vector <int> ans;
+        
+        for (int i = 0 ; i <k ; i++){
+            st.insert(arr[i]);             //insert every num in st set
+        }
+        
+        ans.push_back(*st.rbegin());      //st.rbegin ->  returns a reverse iterator pointing to the last element in the container.
+        
+        for (int i = k; i<n ; i++){         //k is size of array
+            st.erase(st.find(arr[i-k]));     //erase i-k 
+            st.insert(arr[i]);
+            
+            ans.push_back(*st.rbegin());   //push_back() function is used to push elements into a vector from the back.
+        }
+        return ans;
+    }
+};
