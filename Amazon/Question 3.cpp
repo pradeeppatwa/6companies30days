f4cd19e4be7b46484f43cee54b4b33fc2e9671c8
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-09 16:38:01 +0530

    added solution

--- Amazon/Question 3.cpp
+++ Amazon/Question 3.cpp
@@ -24,3 +24,30 @@ class Solution {
         return ans;
     }
 };
+
+
+
+/*
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+       vector<int> ans;
+        deque<int> dq;
+        int i=0;
+        while(i < n){
+            while(!dq.empty() and dq.front() == i-k) dq.pop_front();
+            
+            while(!dq.empty() and arr[dq.back()] < arr[i]) dq.pop_back();
+            
+            dq.push_back(i);
+            
+            if(i >= k-1) ans.push_back(arr[dq.front()]);
+            
+            i++;
+        }
+        return ans;
+    }
+};
+
+*/
