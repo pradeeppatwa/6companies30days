ae69978fa48559191c3ae57d4a540529738b16b5
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-19 16:21:52 +0530

    Create code5.cpp

---
+++ Microsoft/code5.cpp
@@ -0,0 +1,26 @@
+// Stock span problem
+
+vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       stack<pair<int, int>> st;
+      st.push({price[0], 1});
+      vector<int> ans(n, 1);
+      
+      int i=1;
+      while(i<n)
+      {
+          int cur=price[i];
+          int c=0;
+          while(!st.empty() && st.top().first<=cur)
+          {
+              c+=st.top().second;
+              st.pop();
+          }
+          ans[i]+=c;
+          st.push({cur, ans[i]});
+          i++;
+      }
+      
+      return ans;
+    }
