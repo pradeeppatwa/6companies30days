6a3d35d69413afb736706447e57ea216dfa353cc
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-26 23:57:03 +0530

    Update code02.cpp

--- Adobe/code02.cpp
+++ Adobe/code02.cpp
@@ -1,1 +1,35 @@
 //Longest Arithmetic Progression
+//https://practice.geeksforgeeks.org/problems/longest-arithmetic-progression1019/1/
+
+class Solution{   
+public:
+    int lengthOfLongestAP(int A[], int n) {
+        // code here
+           if(n==1) return 1;
+       
+       if(n==2) return 2;
+       
+       vector<unordered_map<int,int>> dp(n);
+       int ans = 0;
+       for(int i=1;i<n;i++)
+       {
+           
+           for(int j=0;j<i;j++)
+           {
+               int diff = A[i] - A[j];
+               
+               if(dp[j].find(diff)==dp[j].end())
+               {
+                   dp[i][diff] = 2;
+               }else
+               {
+                   dp[i][diff] = dp[j][diff]+1;
+               }
+               ans = max(ans,dp[i][diff]);
+               
+           }
+       }
+      
+       return ans;
+    }
+};
