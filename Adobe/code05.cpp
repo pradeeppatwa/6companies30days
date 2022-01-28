3643640843cb638598482e57eef03f53004a6a30
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-29 00:21:44 +0530

    Update code05.cpp

--- Adobe/code05.cpp
+++ Adobe/code05.cpp
@@ -1,1 +1,21 @@
+// Express as sum of power of natural numbers
 
+long long numOfWays(int n, int x)
+    {
+        long long dp[n+1];
+        memset(dp, 0, sizeof(dp));
+        
+        dp[0] = dp[1] = 1;
+        
+        int maxLimit = pow(n, 1.0/x);
+
+        for(int i = 2; i <= maxLimit; i++){
+            int curr = pow(i, x);
+            
+            for(int j = n; j >= curr; j--){
+                dp[j] += dp[j-curr];
+            }
+        }
+        
+        return dp[n];
+    }
