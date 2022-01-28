1a57c6ec22bfc8233448b5600a069d40de6ca7cf
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-29 00:22:24 +0530

    Update code06.cpp

--- Adobe/code06.cpp
+++ Adobe/code06.cpp
@@ -1,1 +1,27 @@
+// Generate Parentheses
 
+vector<string>vs;
+   void sol(string op,int open,int close){
+       if(open==0 && close==0){
+           vs.push_back(op);
+           return;
+       }
+       if(open!=0){
+           string op1=op;
+           op1.push_back('(');
+           sol(op1,open-1,close);
+       }
+       if(close>open){
+           string op2=op;
+           op2.push_back(')');
+           sol(op2,open,close-1);
+       }
+       return ;
+   }
+    vector<string> AllParenthesis(int n) 
+    {
+        // Your code goes here 
+        string s;
+       sol(s,n,n);
+       return vs;
+    }
