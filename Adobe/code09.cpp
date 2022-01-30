0e283cd597ddf723c9473c5ce7d99d65da6e12ca
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-30 20:50:16 +0530

    Update code09.cpp

--- Adobe/code09.cpp
+++ Adobe/code09.cpp
@@ -1,1 +1,20 @@
+// Next higher palindromic number using the same set of digits
 
+string nextPalin(string s) { 
+        //complete the function here
+        int n=s.size();
+        if(n<=3) return "-1";
+        string half = s.substr(0,n/2);
+        if( next_permutation( half.begin() , half.end() ) )
+        {
+            string ans=half;
+            if(n&1)
+            {
+                ans+=s[n/2];
+            }
+            reverse(half.begin() , half.end());
+            ans+=half;
+            return ans;
+        }
+        return "-1";
+    }
