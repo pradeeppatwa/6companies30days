9ad6f39968afe08cb07135ad54e17dc0f88c8be0
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-01-31 18:19:47 +0530

    Update code12.cpp

--- Adobe/code12.cpp
+++ Adobe/code12.cpp
@@ -1,1 +1,17 @@
+// Leaders in an array
 
+vector<int> leaders(int a[], int n){
+        // Code here
+          vector<int> v;
+       int leader=a[n-1];
+       v.push_back(leader);
+       for(int i=n-2;i>=0;i--){
+           if(a[i]>=leader){
+               leader=a[i];
+               v.push_back(leader); 
+           }
+       }
+      
+       sort(v.begin(),v.end(), greater<int>());
+       return v;
+    }
