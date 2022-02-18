e4a113b69c054d9ba4c289947cb7037a4bde76fd
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-02-18 22:39:01 +0530

    Update code04.cpp

--- Walmart/code04.cpp
+++ Walmart/code04.cpp
@@ -1,1 +1,15 @@
+// Number of Unique Paths
 
+class Solution
+{
+    public:
+    //Function to find total number of unique paths.
+    int NumberOfPath(int a, int b)
+    {
+        //code here
+        if(a==1 || b==1)
+            return 1;
+        
+        return NumberOfPath(a-1,b)+NumberOfPath(a,b-1);
+    }
+};
