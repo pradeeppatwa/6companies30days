8fe3a8bb647fb47bc29c2d42aea03ba32bdc1865
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-02-19 23:53:18 +0530

    Update code05.cpp

--- Walmart/code05.cpp
+++ Walmart/code05.cpp
@@ -1,1 +1,35 @@
+// Transform to Sum Tree
 
+class Solution {
+  public:
+  
+    // Convert a given tree to a tree where every node contains sum of values of
+    // nodes in left and right subtrees in the original tree
+    void toSumTree(Node *root)
+    {
+        // Your code here
+        if(!root)
+           return;
+       
+       int lb=0;
+       int la=0;
+       if(root->left)
+       {
+           lb=root->left->data;
+           toSumTree(root->left);
+           la=root->left->data;
+       }
+       
+       int rb=0;
+       int ra=0;
+       if(root->right)
+       {
+           rb=root->right->data;
+           toSumTree(root->right);
+           ra=root->right->data;
+       }
+       
+       root->data= rb + ra + la + lb;
+       return;
+    }
+};
