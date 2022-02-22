a3b2e7261a5b6ab7d1b143e0720c8809d76a0d1b
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-02-22 23:27:05 +0530

    Update code06.cpp

--- Walmart/code06.cpp
+++ Walmart/code06.cpp
@@ -1,1 +1,25 @@
+// Power Of Numbers
 
+ long long power(int N,int R)
+   {
+     
+
+   if(R==0)
+   {
+       return 1;
+   }
+   
+   long long k=power(N,R/2)%1000000007;
+   long long k2=(k*k)%1000000007;
+   
+   if(R&1)
+   {
+       return ((N*k2)% 1000000007);
+   }
+   
+   
+   
+   return k2 % 1000000007;
+   
+  
+   }
