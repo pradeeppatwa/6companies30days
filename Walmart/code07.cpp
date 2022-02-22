a308bba4843e1d770e7f6a3761d91bdbc076acd3
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-02-22 23:27:43 +0530

    Update code07.cpp

--- Walmart/code07.cpp
+++ Walmart/code07.cpp
@@ -1,1 +1,19 @@
+// Sorted subsequence of size 3
 
+vector<int> find3Numbers(vector<int> a, int n) {
+       stack<int> s;
+       for(int i=n-1;i>=0;i--){
+           while(!s.empty()&&s.top()<=a[i])//Pop the elements that are smaller than current number
+           s.pop();    
+           s.push(a[i]); //push every element since it will always be smaller than stacks top
+           if(s.size()==3)break;   //we found answer
+       }
+       vector<int> ans;
+       while(!s.empty()){
+           ans.push_back(s.top());
+           s.pop();
+       }
+       if(ans.size()!=3)return {}; // when we don't get three numbers
+       return ans;
+   
+   }
