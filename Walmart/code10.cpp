60b09917ce596349883b03eb2f664cfaae909b79
Pradeep Patwa <65440507+pradeeppatwa@users.noreply.github.com>
2022-02-26 17:13:12 +0530

    Update code10.cpp

--- Walmart/code10.cpp
+++ Walmart/code10.cpp
@@ -1,1 +1,20 @@
+// Generate Random Point in a Circle
 
+class Solution {
+public:
+    double x;
+    double y;
+    double r;
+    Solution(double radius, double x_center, double y_center) {
+        x = x_center;
+        y = y_center;
+        r = radius;
+    }
+    vector<double> randPoint() {
+        double rad = (double)rand() / RAND_MAX;
+        rad = sqrt(rad) * r; 
+        double teta = (double)rand() / RAND_MAX;
+        teta *= 360.0;
+        return {x+rad*cos(teta),y+rad*sin(teta)}; //x = rcosθ, y = rsinθ
+    }
+};
